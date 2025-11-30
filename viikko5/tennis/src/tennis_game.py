dcd10eb4dee580a1c7b5e2fc8f7445a115a8378a
Antti Moisio <moisioa1@gmail.com>
2025-11-30 09:05:49 +0200

    Fixed scoring issue if wrong player name is given

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -10,7 +10,7 @@ class TennisGame:
     def won_point(self, player_name):
         if player_name == self.player1_name:
             self.player1_score = self.player1_score + 1
-        else:
+        elif player_name == self.player2_name:
             self.player2_score = self.player2_score + 1
 
     def get_score(self):
