c0f512ac9544f249e9137481fe9e4ce1803bd570
Antti Moisio <moisioa1@gmail.com>
2025-12-04 12:12:16 +0200

    Task 1 done

---
+++ viikko6/laskin/src/komennot.py
@@ -0,0 +1,31 @@
+class Summa:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        return self._sovelluslogiikka.plus(int(self._io()))
+    
+class Erotus:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        return self._sovelluslogiikka.miinus(int(self._io()))
+    
+class Nollaus:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        return self._sovelluslogiikka.nollaa(self._io())
+    
+class Kumoa:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        return
